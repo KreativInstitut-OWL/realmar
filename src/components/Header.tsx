6d5571fd5ecb319c36dfb101252741f69d2720e9
Conrad Dreyer <git@conraddreyer.com>
2024-05-08 19:03:35 +0200

    add change language

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,29 @@
 import kiologo from "/kio_logo_aufweiss.svg";
+import { useLanguage } from "../LanguageProvider";
 function Header() {
+  const { language, setLanguage } = useLanguage();
+
   return (
     <header>
       <img src={kiologo} alt="KIO Logo" />
+      <ul className="language-options">
+        <li>
+          <button
+            onClick={() => setLanguage("en")}
+            className={language == "en" ? "active" : ""}
+          >
+            English
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => setLanguage("de")}
+            className={language == "de" ? "active" : ""}
+          >
+            Deutsch
+          </button>
+        </li>
+      </ul>
       <h1>BundlAR</h1>
     </header>
   );
