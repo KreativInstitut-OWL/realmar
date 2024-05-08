72a9dc772845d9dc30e61b2ca5523dc15fe86c79
Conrad Dreyer <git@conraddreyer.com>
2024-05-08 19:11:42 +0200

    fix tests to work with languageprovider

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { LanguageProvider } from "./LanguageProvider";
 
 describe("App", () => {
   it("renders the App component", () => {
-    render(<App />);
+    render(
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>,
+    );
     const appText = screen.getByText("BundlAR");
     expect(appText).toBeInTheDocument();
   });
--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import { LanguageProvider } from "../LanguageProvider";
 
 test("renders header", () => {
-  render(<Header />);
+  render(
+    <LanguageProvider>
+      <Header />
+    </LanguageProvider>,
+  );
   const headerElement = screen.getByAltText("KIO Logo");
   expect(headerElement).toBeInTheDocument();
 });
